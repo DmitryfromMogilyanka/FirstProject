3c4eb07931df3ad2f3319b6785aa058660468c1d
DmitryfromMogilyanka <fildima45@gmail.com>
2020-12-02 22:56:07 +0200

    add new file Program Welcome

---
+++ Program 'Welcome'.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int password;
+    cout << "Input Your password ";
+    cin >> password;
+
+    if (password == 12345) {
+    
+        cout << "Welcome Dmitry";
+    }
+    else {
+    
+        cout << "Invalid user";
+    }
+
+}
